5737b68fe602cb6c800fe37e6c64c817264535b4
Zoltan Szucs <zz.szucs@gmail.com>
2026-05-08 21:59:06 +0000

    fix(platform_language): handle null locale and return default language

--- shared/ion/src/simulator/linux/platform_language.cpp
+++ shared/ion/src/simulator/linux/platform_language.cpp
@@ -9,12 +9,13 @@ namespace Platform {
 const char* languageCode() {
   static char buffer[3] = {0};
   char* locale = setlocale(LC_ALL, "");
-  if (locale[2] == '_') {
+  if (locale != nullptr && locale[2] == '_') {
     buffer[0] = locale[0];
     buffer[1] = locale[1];
     return buffer;
   }
-  return nullptr;
+  // Return default language "en" when locale is unavailable
+  return "en";
 }
 
 }  // namespace Platform
